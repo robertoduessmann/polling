cf6fa1345e1a66ff533529846cd1ce84d20a5e3c
Roberto Duessmann <roberto.duessmann@gmail.com>
2017-11-22 22:13:32 -0200

    Move keypress listener to template events

--- client/main.js
+++ client/main.js
@@ -31,13 +31,3 @@ UI.registerHelper('indexedArray', function(context, options) {
     });
   }
 });
-
-/*validation for not to allow space in name field */
-$(function() {
-  $('#nome').on('keypress', function(e) {
-      if (e.which == 32){
-          alert("Please use Numbers and Letters Only!!");
-          return false;
-      }
-  });
-})
